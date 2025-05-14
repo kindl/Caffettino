71b1e3f1253068ff2de86468fc345cf41d1f8856
kindl <kindl@users.noreply.github.com>
2025-05-14 13:38:21 +0200

    Basic jar creation

--- src/Main.kt
+++ src/Main.kt
@@ -1,17 +1,63 @@
+import java.io.FileOutputStream
 import java.nio.file.Files
-import java.nio.file.Paths
+import java.nio.file.Path
+import java.util.jar.Attributes
+import java.util.jar.JarEntry
+import java.util.jar.JarOutputStream
+import java.util.jar.Manifest
 
-fun generate(out: String, file: String, expressions: List<Expression>) {
+
+fun generate(file: String, text: String): ByteArray {
     println("Generating " + file)
-    val annotated = annotateFile(file, expressions)
+    val parsed = parseString(text)
+    val annotated = annotateFile(file, parsed)
     val resolved = resolveFile(annotated)
     val generated = generateClassFile(file, resolved)
+    return generated
+}
 
-    val outPath = Paths.get(out)
+fun generateAndWriteClassFile(out: String, file: String, text: String) {
+    val generated = generate(file, text)
+    val outPath = Path.of(out)
     Files.createDirectories(outPath)
-    val writePath = Paths.get(out + "/" + file + ".class")
+    val writePath = Path.of(out + "/" + file + ".class")
     Files.write(writePath, generated)
 }
 
+fun generateAndWriteJar(out: String, file: String, text: String) {
+    val generated = generate(file, text)
+
+    val manifest = Manifest()
+    manifest.mainAttributes.put(Attributes.Name.MANIFEST_VERSION, "1.0")
+    // only needs class name, no ".class"
+    manifest.mainAttributes.put(Attributes.Name.MAIN_CLASS, file)
+
+    val writePath = out + "/" + file + ".jar"
+    val jarOutputStream = JarOutputStream(FileOutputStream(writePath), manifest)
+    val entry = JarEntry(file + ".class")
+    jarOutputStream.putNextEntry(entry)
+    jarOutputStream.write(generated)
+    jarOutputStream.closeEntry()
+
+    // TODO add imported classes to archive
+
+    jarOutputStream.close()
+}
+
 fun main(args: Array<String>) {
+    val command = args.firstOrNull()
+
+    if (command == "compile") {
+        val file = args[1]
+        val path = Path.of(file)
+        val text = Files.readString(path)
+        generateAndWriteClassFile(".", file, text)
+    } else if (command == "build") {
+        val file = args[1]
+        val path = Path.of(file)
+        val text = Files.readString(path)
+        generateAndWriteJar(".", file, text)
+    } else {
+        println("Unknown command " + command)
+    }
 }
